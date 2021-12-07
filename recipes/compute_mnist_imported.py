85f01b518def18b04451ad78bded76110dee78b0
admin <atchao@sce.pccu.edu.tw>
2021-12-07 09:03:36 +0800

    Saved recipe: HANDWRITING.compute_mnist_imported

---
+++ recipes/compute_mnist_imported.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+mnist = dataiku.Folder("")
+mnist_info = mnist.get_info()
+
+
+# Compute recipe outputs
+# TODO: Write here your actual code that computes the outputs
+# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
+
+mnist_imported_df = ... # Compute a Pandas dataframe to write into mnist_imported
+
+
+# Write recipe outputs
+mnist_imported = dataiku.Dataset("mnist_imported")
+mnist_imported.write_with_schema(mnist_imported_df)
