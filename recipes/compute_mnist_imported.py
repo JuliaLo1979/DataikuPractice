bcb87ddca24c9004ecd1593df6b38ec1d96f3753
admin <atchao@sce.pccu.edu.tw>
2021-12-07 09:12:44 +0800

    Saved recipe: HANDWRITING.compute_mnist_imported

--- recipes/compute_mnist_imported.py
+++ recipes/compute_mnist_imported.py
@@ -1,20 +1,38 @@
 # -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+#import dataiku
+#import pandas as pd, numpy as np
+#from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-mnist = dataiku.Folder("UVAoMwxi")
-mnist_info = mnist.get_info()
+#mnist = dataiku.Folder("UVAoMwxi")
+#mnist_info = mnist.get_info()
 
 
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-mnist_imported_df = pd.DataFrame(columns=['path']) # Compute a Pandas dataframe to write into mnist_imported
+#mnist_imported_df = pd.DataFrame(columns=['path']) # Compute a Pandas dataframe to write into mnist_imported
 
 
 # Write recipe outputs
-mnist_imported = dataiku.Dataset("mnist_imported")
-mnist_imported.write_with_schema(mnist_imported_df)
+#mnist_imported = dataiku.Dataset("mnist_imported")
+#mnist_imported.write_with_schema(mnist_imported_df)
+###
+
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+folder = dataiku.Folder("UVAoMwxi")
+
+# Initialize data frame
+df = pd.DataFrame(columns=['path'])
+
+# Populate dataframe with paths
+df['path'] = folder.list_paths_in_partition()
+
+# Write recipe outputs
+mnist = dataiku.Dataset("mnist")
+mnist.write_with_schema(df)
